cb201216003de2d04f8b4313c077c13eaa119db7
Stephen Joy <stephenjoy98@gmail.com>
2022-06-21 21:57:06 -0700

    SpringBoot - JAR template

---
+++ src/main/java/com/csis3275/pwa/PwaApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275.pwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csis3275/pwa/PwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275.pwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
